ac6d37a6d2684c67afcddc706c95c437f2439eac
Rosemilson <rozbr96@gmail.com>
2024-01-12 15:51:10 -0300

    Implementa listagem de receitas favoritas na home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @recipes = Recipe.all
     @recipe_types = RecipeType.all
+    if current_user
+      @favorite_recipes = current_user.favorite_recipes.order(created_at: :desc).limit 5
+    end
   end
 end
--- spec/features/user_views_favorite_recipes_spec.rb
+++ spec/features/user_views_favorite_recipes_spec.rb
@@ -35,4 +35,34 @@ describe 'Usuário vê receitas que favoritou' do
     expect(page).not_to have_content 'Beijinho'
     expect(page).not_to have_content 'Pudim de Leite'
   end
+
+  it 'e vê as últimas 5 receitas favoritadas na página inicial' do
+    user = create(:user)
+    recipe_1 = create(:recipe, title: 'Brigadeiro')
+    recipe_2 = create(:recipe, title: 'Beijinho')
+    recipe_3 = create(:recipe, title: 'Pudim de Leite')
+    recipe_4 = create(:recipe, title: 'Sorvete')
+    recipe_5 = create(:recipe, title: 'Hambúrguer')
+    recipe_6 = create(:recipe, title: 'Cocada')
+
+    create(:favorite_recipe, user: user, recipe: recipe_1)
+    create(:favorite_recipe, user: user, recipe: recipe_2)
+    create(:favorite_recipe, user: user, recipe: recipe_3)
+    create(:favorite_recipe, user: user, recipe: recipe_4)
+    create(:favorite_recipe, user: user, recipe: recipe_5)
+    create(:favorite_recipe, user: user, recipe: recipe_6)
+
+    login_as user
+    visit root_path
+
+    within '#favorite_recipes' do
+      expect(page).to have_content 'Últimas Receitas Favoritadas'
+      expect(page).not_to have_content 'Brigadeiro'
+      expect(page).to have_content 'Beijinho'
+      expect(page).to have_content 'Pudim de Leite'
+      expect(page).to have_content 'Sorvete'
+      expect(page).to have_content 'Hambúrguer'
+      expect(page).to have_content 'Cocada'
+    end
+  end
 end
